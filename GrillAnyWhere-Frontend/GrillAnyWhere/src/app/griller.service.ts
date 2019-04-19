8b8c3b9100aaa403560ea642341a83d8efe847bb
Lavanya Kantamani <kanlava1@in.ibm.com>
2019-04-19 20:42:32 +0530

    created new branch

--- GrillAnyWhere-Frontend/GrillAnyWhere/src/app/griller.service.ts
+++ GrillAnyWhere-Frontend/GrillAnyWhere/src/app/griller.service.ts
@@ -1,23 +1,46 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient,HttpHeaders} from "@angular/common/http";
+import { RequestOptions } from '@angular/http';
 import { error } from '@angular/compiler/src/util';
-
-
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { ContentType } from '@angular/http/src/enums';
+//import {angular} from "angular";
+//import  { angular } from "angular";
 @Injectable({
   providedIn: 'root'
 })
 export class GrillerService {
-
-  private _url:string = 'http://localhost:8080//grillAnywhere/griller'
-  constructor(private http:HttpClient) { }
-
+private _url:string = 'http://localhost:8080//grillAnywhere/griller';
+  //private file:File
+  //private formData: FormData = new FormData();
+  private config:any
+  private array:any[]
+  private userForm:FormGroup
   
-   createUser(userObj:any,callback){
+  constructor(private http:HttpClient,private fb:FormBuilder) { }
+  createUser(userObj:any,callback){
     
-     console.log(JSON.stringify(userObj));
+    let file: File =userObj.grillImage;
+        let formData:FormData = new FormData();
+      //this.file = userObj.grillImage
+    console.log(userObj.grillImage);
+    formData.append('file',userObj.grillImage);
+    let headers: Headers = new Headers()
+    headers.append('Content-Type', 'multipart/form-data;boundary=imageUpload');
+    //formData.append('Content-Type', undefined);
+    //this.formData.append("grillName",userObj.grillName);
+    console.log(formData.get("file"));
+    // let headers = new Headers();
+    //     headers.append('Content-Type', undefined);
+    //     headers.append('Accept', 'application/json');
+    //     let options = new RequestOptions({ headers: headers });
+    //  console.log(JSON.stringify(userObj));
     this.http
-    .post(this._url,userObj).subscribe(response=>{
-      console.log("sercice")
+    .post(this._url,formData,{headers:{
+      'Content-Type':'multipart/form-data;boundary=imageUpload'
+  }}).subscribe(response=>{
+      
+      console.log("service")
       callback(null)
     },error=>{
       callback(error)
@@ -34,6 +57,26 @@ console.log('unable to process request')
 
      })
    }
+
+   deleteUser(grillId,callback){
+    this.http.delete(this._url+"/"+grillId).subscribe(data=>{
+callback(data)
+console.log(data)},error=>{
+
+console.log('unable to process request')
+
+    })
+  }
+  getGrillById(grillId,callback){
+    this.http.get(this._url+"/byID/"+grillId).subscribe(data=>{
+callback(data)
+console.log(data)},error=>{
+
+console.log('unable to process request')
+
+    })
+  }
+
 
    getGrillerAutomatic(callback){
     this.http.get(this._url+"/byGrillerType/AutomaticGriller").subscribe(data=>{
@@ -64,9 +107,11 @@ console.log('unable to process request')
       grillerType: user.grillerType,
       location:user.location,
       grillerDescriptions:user.grillerDescriptions,
-      price:user.price
+      price:user.price,
+      grillImage:user.grillImage
       
     }
+    
     this.createUser(userObj,(err)=>{
       callback(err)
     })
